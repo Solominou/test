44ef5a2f1cb243012876ba251dbbddd0e7b68e9c
solominou <96089086+Solominou@users.noreply.github.com>
2023-04-13 11:51:26 +0300

    Update EgyBest.kt

--- TestPlugin/src/main/kotlin/com/test/EgyBest.kt
+++ TestPlugin/src/main/kotlin/com/test/EgyBest.kt
@@ -48,7 +48,7 @@ class EgyBest : MainAPI() {
 	
     private fun Element.toSearchResponse(): SearchResponse? {
         val url = this.attr("href") ?: return null
-        val posterUrl = select("img").attr("src")
+        val posterUrl = select("div").attr("src")
         var title = select("span.title").text()
         val year = title.getYearFromTitle()
         val isMovie = Regex(".*/movie/.*|.*/masrahiya/.*").matches(url)
